b016ad74f5026dedfc424f8b0cc5291558d22e70
rodrigoalien <rodrigodacunhaalmeida@gmail.com>
2020-02-02 18:27:52 -0300

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Frases',
       theme: ThemeData(
 
         primarySwatch: Colors.deepOrange,
